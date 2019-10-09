dbaae91efed28292ff8e257483838c5a464a7b92
Mateusz Karcz <mateusz.karcz@interia.eu>
2019-10-09 21:51:38 +0200

    Make a little cleanup

--- hello-native/main.c
+++ hello-native/main.c
@@ -1,19 +1,25 @@
+#ifdef ARCH_x86
 #define _X86_
+#endif
+#ifdef ARCH_x64
+#define _AMD64_
+#endif
+
 #include <ntddk.h>
 
 NTSTATUS NTAPI NtDisplayString(PUNICODE_STRING);
 NTSTATUS NTAPI NtDelayExecution(BOOLEAN, PLARGE_INTEGER);
 NTSTATUS NTAPI NtTerminateProcess(HANDLE, NTSTATUS);
 
-#define DELAY_MS(ms) ((ms) * 10000)
+#define DELAY_MS(ms) ((ms) * -10000)
 
-VOID NtProcessStartup(int argc, char** argv) {
+VOID NTAPI NtProcessStartup(PPEB peb) {
     UNICODE_STRING hello, dot;
     LARGE_INTEGER delay;
 
-    RtlInitUnicodeString(&hello, L"Hello, World! Mateusz sends greetings");
+    RtlInitUnicodeString(&hello, L"Hello, World! Catkitty sends greetings");
     RtlInitUnicodeString(&dot, L".");
-    delay.QuadPart = -DELAY_MS(1000);
+    delay.QuadPart = DELAY_MS(1000);
 
     NtDisplayString(&hello);
     NtDisplayString(&dot);
