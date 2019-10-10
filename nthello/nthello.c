551b9a208341d0d90cbcbc5e28b3bd296315a785
Mateusz Karcz <mateusz.karcz@interia.eu>
2019-10-10 23:17:00 +0200

    Open the BVidCelo device

--- nthello/nthello.c
+++ nthello/nthello.c
@@ -7,9 +7,14 @@
 
 #include <ntddk.h>
 
-NTSTATUS NTAPI NtDisplayString(PUNICODE_STRING);
-NTSTATUS NTAPI NtDelayExecution(BOOLEAN, PLARGE_INTEGER);
-NTSTATUS NTAPI NtTerminateProcess(HANDLE, NTSTATUS);
+NTSTATUS NTAPI NtDisplayString(
+    IN PUNICODE_STRING Message);
+NTSTATUS NTAPI NtDelayExecution(
+    IN BOOLEAN Alertable,
+    IN PLARGE_INTEGER DelayInterval);
+NTSTATUS NTAPI NtTerminateProcess(
+    IN HANDLE ProcessHandle,
+    IN NTSTATUS ExitStatus);
 
 #define DELAY_MS(ms) ((ms) * -10000)
 
@@ -18,6 +23,25 @@ VOID NTAPI NtProcessStartup(
 {
     UNICODE_STRING hello, dot;
     LARGE_INTEGER delay;
+
+    NTSTATUS status;
+    HANDLE file;
+    OBJECT_ATTRIBUTES oattrs;
+    IO_STATUS_BLOCK iosb;
+    UNICODE_STRING device = RTL_CONSTANT_STRING(L"\\Device\\BVidCelo");
+    InitializeObjectAttributes(
+        &oattrs,
+        &device,
+        OBJ_OPENIF,
+        NULL,
+        NULL);
+    status = ZwOpenFile(
+        &file,
+        GENERIC_READ | GENERIC_WRITE,
+        &oattrs,
+        &iosb,
+        0,
+        FILE_OPEN);
 
     RtlInitUnicodeString(&hello, L"Hello, World! Catkitty sends greetings");
     RtlInitUnicodeString(&dot, L".");
@@ -35,5 +59,8 @@ VOID NTAPI NtProcessStartup(
     NtDisplayString(&dot);
     NtDelayExecution(FALSE, &delay);
 
+    if (NT_SUCCESS(status)) {
+        ZwClose(file);
+    }
     NtTerminateProcess((HANDLE)-1, 0);
 }
