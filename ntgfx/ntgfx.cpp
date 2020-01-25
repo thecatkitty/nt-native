b8a7f3836e52582e3ded8b11a2f4fce0f36ddb7d
Mateusz Karcz <mateusz.karcz@interia.eu>
2020-01-25 01:20:01 +0100

    Display "Hello World" message in NTGfx, together with process parameters

--- ntgfx/ntgfx.cpp
+++ ntgfx/ntgfx.cpp
@@ -2,6 +2,15 @@
 #include <canter/process.hpp>
 
 
+#define BUFFER_LENGTH 100
+
+
+static VOID UnicodeToAscii7(
+    IN PUNICODE_STRING unicode,
+    OUT PSTR ascii7,
+    IN USHORT length);
+
+
 VOID NTAPI NtProcessStartup(
     IN PPEB peb)
 {
@@ -12,13 +21,48 @@ VOID NTAPI NtProcessStartup(
 
     auto status = vid.open(
         canter::access::read | canter::access::write);
-    if (status == STATUS_SUCCESS)
+    if (NT_SUCCESS(status))
     {
-        canter::process::delay(CANTER_DURATION_MS(1000));
+        char msg[] = "Hello World!\nI love programming!\n";
+        char nl[] = "\n";
+        char buff[BUFFER_LENGTH];
+
+        vid.write(msg, sizeof(msg));
+        canter::process::delay(CANTER_DURATION_MS(1500));
+
+        UnicodeToAscii7(
+            canter::process::get_path(peb),
+            buff,
+            BUFFER_LENGTH);
+        vid.write(buff, BUFFER_LENGTH);
+        vid.write(nl, sizeof(nl));
+        canter::process::delay(CANTER_DURATION_MS(1500));
+
+        UnicodeToAscii7(
+            canter::process::get_command_line(peb),
+            buff,
+            BUFFER_LENGTH);
+        vid.write(buff, BUFFER_LENGTH);
+        canter::process::delay(CANTER_DURATION_MS(1500));
+
         vid.close();
 
         canter::process::terminate(STATUS_SUCCESS);
     }
 
-    canter::process::terminate(STATUS_SUCCESS);
+    canter::process::terminate(status);
+}
+
+
+VOID UnicodeToAscii7(
+    IN PUNICODE_STRING unicode,
+    OUT PSTR ascii7,
+    IN USHORT length)
+{
+    USHORT i;
+    for (i = 0; (i < unicode->Length) && (i < length - 1); i++)
+    {
+        ascii7[i] = unicode->Buffer[i] > 0x7F ? '?' : (CHAR)unicode->Buffer[i];
+    }
+    ascii7[i] = 0;
 }
