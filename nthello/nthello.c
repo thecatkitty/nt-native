163519e2b0fdfeb55ab305cfdb74d100f9f58a86
Mateusz Karcz <mateusz.karcz@interia.eu>
2020-01-28 21:23:09 +0100

    Use the code from the report for Hello World program

--- nthello/nthello.c
+++ nthello/nthello.c
@@ -1,30 +1,37 @@
-#include <platform.h>
-#include <ntddk.h>
-#include <dietndk/nt.h>
+#ifdef ARCH_x86
+#ifndef _X86_
+#define _X86_
+#endif
+#endif // ARCH_x86
 
-#define DELAY_MS(ms) ((ms) * -10000)
+#ifdef ARCH_x64
+#ifndef _AMD64_
+#define _AMD64_
+#endif
+#endif // ARCH_x64
+
+#include <ntddk.h>
+
+NTSYSCALLAPI NTSTATUS NTAPI NtDisplayString(
+    IN PUNICODE_STRING DisplayString);
+
+NTSYSCALLAPI NTSTATUS NTAPI NtDelayExecution(
+    IN BOOLEAN Alertable,
+    IN LARGE_INTEGER* Interval);
+
+NTSYSCALLAPI NTSTATUS NTAPI NtTerminateProcess(
+    IN HANDLE ProcessHandle,
+    IN NTSTATUS ExitStatus);
+
 
 VOID NTAPI NtProcessStartup(
     IN PPEB peb)
 {
-    UNICODE_STRING hello, dot;
+    UNICODE_STRING hello;
     LARGE_INTEGER delay;
-
-    RtlInitUnicodeString(&hello, L"Hello, World! Catkitty sends greetings");
-    RtlInitUnicodeString(&dot, L".");
-    delay.QuadPart = DELAY_MS(1000);
-
+    RtlInitUnicodeString(&hello, L"Hello World!");
     NtDisplayString(&hello);
-    NtDisplayString(&dot);
+    delay.QuadPart = -50000000;
     NtDelayExecution(FALSE, &delay);
-    NtDisplayString(&dot);
-    NtDelayExecution(FALSE, &delay);
-    NtDisplayString(&dot);
-    NtDelayExecution(FALSE, &delay);
-    NtDisplayString(&dot);
-    NtDelayExecution(FALSE, &delay);
-    NtDisplayString(&dot);
-    NtDelayExecution(FALSE, &delay);
-
-    NtTerminateProcess((HANDLE)-1, 0);
+    NtTerminateProcess((HANDLE)-1, STATUS_SUCCESS);
 }
